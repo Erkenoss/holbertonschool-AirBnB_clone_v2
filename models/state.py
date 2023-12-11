6cc4562878f3eddbe53a368bdf5a626dfce65bb5
Erkenoss <xavier.bertin6@gmail.com>
2023-12-11 15:42:04 +0100

    fixed state meethode

--- models/state.py
+++ models/state.py
@@ -22,8 +22,4 @@ class State(BaseModel, Base):
             """ Getter attribute, return the list
             of city instance in state.id """
             from models import storage
-            city_list = []
-            for city in storage.all("City").values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            return [city for city in storage.all(City).values() if city.state_id == self.id]
