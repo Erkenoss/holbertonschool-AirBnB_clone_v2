7c28c8e7519dd56abd4a4a4e0836f920d344f80a
Erkenoss <xavier.bertin6@gmail.com>
2023-12-12 13:53:01 +0100

    add task 8-

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Create server with default main page """
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def afterRequest(self):
+    storage.close()
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_list():
+    """def cities_list"""
+    from models.state import State
+    from models.city import City
+
+    states = storage.all(State).values()
+
+
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
