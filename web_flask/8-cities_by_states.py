c77aee95bf4b98ab4885e6f89dc53b687ac1aee5
Erkenoss <xavier.bertin6@gmail.com>
2023-12-12 14:18:39 +0100

    fixed task 8-

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,18 +6,24 @@ from models import storage
 app = Flask(__name__)
 
 
+@app.route("/", strict_slashes=False)
+def index():
+    """def index"""
+    return "Hello HBNB!"
+
+
 @app.teardown_appcontext
 def afterRequest(self):
     storage.close()
 
 
 @app.route("/cities_by_states", strict_slashes=False)
-def cities_list():
-    """def cities_list"""
+def cities_by_states():
+    """def cities_by_states"""
     from models.state import State
     states = []
     for state in storage.all(State).values():
-        states.append({state.to_dict(), {'cities': state.cities}})
+        states.append({**state.to_dict(), **{'cities': state.cities}})
 
     return render_template('8-cities_by_states.html', states=states)
 
