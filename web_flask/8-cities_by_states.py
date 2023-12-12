08202fec7c7ab590a6dceaf3ed78b23f723ff3ef
Erkenoss <xavier.bertin6@gmail.com>
2023-12-12 14:52:44 +0100

    try fixed

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
-""" Create server with default main page """
+"""Web App with Flask"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
+from models.city import City
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def index():
-    """def index"""
-    return "Hello HBNB!"
-
-
 @app.teardown_appcontext
-def afterRequest(self):
+def teardown(exception):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
-@app.route("/cities_by_states", strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """def cities_by_states"""
-    from models.state import State
-    from models.city import City
+    """Display a HTML page with a list of all State objects in DBStorage."""
+    states = storage.all(State)
+    cities = storage.all(City)
 
-    states = storage.all(State).values()
-    cities = storage.all(City).values()
-
-    return render_template('8-cities_by_states.html',
+    return render_template("8-cities_by_states.html",
                            cities=cities,
                            states=states)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
